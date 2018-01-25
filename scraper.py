72c52e30e20ddd4f7c00cb9e7274a3e7599e11d2
acvwebb <35491893+acvwebb@users.noreply.github.com>
2018-01-25 14:24:27 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -11,6 +11,7 @@ html = scraperwiki.scrape("https://uk.soccerway.com/teams/england/chelsea-footba
 root = lxml.html.fromstring(html)
 names = root.cssselect("td div a")
 for name in names: 
+  print name.text
   print name.attrib['href']
 
 #
